8b3a6748bb2e70ad6c60cacf079d3165d7ecc84a
Moh. Ibnu Hablillah <61903019+ibnuhablillah@users.noreply.github.com>
2020-07-10 22:55:03 +0700

    edit main activity

--- Covid19_mohibnuhablilah/app/src/main/java/com/example/covid19_mohibnuhablilah/MainActivity.kt
+++ Covid19_mohibnuhablilah/app/src/main/java/com/example/covid19_mohibnuhablilah/MainActivity.kt
@@ -2,10 +2,58 @@ package com.example.covid19_mohibnuhablilah
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.covid19_mohibnuhablilah.retrofit.ApiService
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        getData()
+    }
+
+    private fun getData(){
+        showLoading(loading = true)
+        ApiService.endpoint.getData()
+            .enqueue(object : Callback<List<MainModel>>{
+                override fun onFailure(call: Call<List<MainModel>>, t: Throwable) {
+                    showLoading(false)
+                }
+
+                override fun onResponse(
+                    call: Call<List<MainModel>>,
+                    response: Response<List<MainModel>>
+                ) {
+                    showLoading(false)
+                    if (response.isSuccessful){
+                        val mainModel: List<MainModel> = response.body()!!
+                        setResponse(mainModel)
+
+                    }
+                }
+
+            })
+    }
+
+    private fun setResponse (mainModel: List<MainModel>){
+        val response = mainModel[0]
+        textViewResult.setText(
+            "Positif: ${response.positif} \nSembuh: ${response.sembuh} \nMeninggal: ${response.meninggal}"
+        )
+    }
+
+    private fun showLoading (loading: Boolean){
+        when (loading) {
+            true -> progressBar.visibility = View.VISIBLE
+            false -> progressBar.visibility = View.GONE
+        }
     }
 }
