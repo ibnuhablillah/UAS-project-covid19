d8227c7d99c7f7ccf2192c43c324726ee9d962d2
Moh. Ibnu Hablillah <61903019+ibnuhablillah@users.noreply.github.com>
2020-07-19 11:15:20 +0700

    edit ApiEndPoint

--- Covid19_mohibnuhablilah/app/src/main/java/com/example/covid19_mohibnuhablilah/retrofit/ApiEndpoint.kt
+++ Covid19_mohibnuhablilah/app/src/main/java/com/example/covid19_mohibnuhablilah/retrofit/ApiEndpoint.kt
@@ -1,14 +1,15 @@
 package com.example.covid19_mohibnuhablilah.retrofit
 
 import android.renderscript.Sampler
-import android.telecom.Call
 import com.example.covid19_mohibnuhablilah.MainModel
+import retrofit2.Call
 import retrofit2.http.GET
 
 interface ApiEndpoint {
 
-    @GET(value = "indonesia")
+    @GET("indonesia")
     fun getData(): Call<List<MainModel>>
+
 
 }
 
